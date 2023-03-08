d0e081aea79ca39f4836b93e3360112efea8d267
tkurisawaya <127141532+tkurisawaya@users.noreply.github.com>
2023-03-08 10:23:01 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,6 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-
 # Display the table on the page.
 
 streamlit.dataframe(fruits_to_show)
@@ -30,7 +29,6 @@ streamlit.write('The user entered ', fruit_choice)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
 
 # take the json version and normalize it 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
